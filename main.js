c087529564360f9eee7dc9cc886f08bb2e74160a
Raul Sofia <rauljcsofia@gmail.com>
2021-05-10 05:53:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 //const dados = getData();
 const alt = window.innerHeight;
 const larg = window.innerWidth;
-console.log(graficos.length);
+//console.log(graficos.length);
 
 //dim = Math.min(alt, larg)
 
+
 for(let i = 0; i < graficos.length; i++) {
-    plotData("plot1", graficos[i].x, graficos[i].y, graficos[i].config, larg , alt);
+    plotData("plot1", graficos[i].x, graficos[i].y, graficos[i].config, larg , alt-10);
 }
