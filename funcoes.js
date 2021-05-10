63e2b234f92ed2a256200a2d5f22f30118db0640
Raul Sofia <rauljcsofia@gmail.com>
2021-05-10 05:53:01 +0100

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -1,69 +1,5 @@
-async function getData() {
-    const response = await fetch("molecules.csv");
-    //console.log(response);
-    const data = await response.text();
-    //console.log(data);
-    const rows = data.split('\n');
-    let dados = {
-        "smiles": [],
-        "pic50": [],
-        "bbb": [],
-        "mw": [],
-        "logp": [],
-        "sas": [],
-        "qed": []
-    }
-    for(let i = 6; i < rows.length - 1; i++) {
-        const row = rows[i].split(',');
-        dados.smiles.push(row[0]);
-        dados.pic50.push(row[1]);
-        dados.bbb.push(row[2]);
-        dados.mw.push(row[3]);
-        dados.logp.push(row[4]);
-        dados.sas.push(row[5]);
-        dados.qed.push(row[6]);
-    }
-    //console.log(dados);
-    return dados;
-}
-
-async function plotData(id, xAxis, yAxis, config, larg, alt) {
-    const dados = await getData();
-    console.log(dados[xAxis], dados[yAxis]);
-    let eixos = [];
-    for(let i = 0; i < dados.smiles.length; i++) {
-        eixos.push({
-        x: dados[xAxis][i],
-        y: dados[yAxis][i]
-        });
-    }
-    console.log(eixos);
-    config_local = JSON.parse(JSON.stringify(config)); //perguntar se é assim q se faz deepcopy
-    config_local.data.datasets[0].data = eixos;
-    config_local.options.scales.x.title.text = xAxis
-    config_local.options.scales.y.title.text = yAxis
-    console.log(config_local);
-    let canvas = document.createElement("CANVAS");
-    canvas.width = larg;
-    canvas.height = alt;
-    document.body.appendChild(canvas);
-    const ctx = canvas.getContext("2d");
-    const plot = new Chart(ctx, config_local);
-}
-
-
-
-
-
-
-//tooltip
-
-
-
-
 const getOrCreateTooltip = (chart) => {
     let tooltipEl = chart.canvas.parentNode.querySelector('div');
-  
     if (!tooltipEl) {
       tooltipEl = document.createElement('div');
       tooltipEl.style.background = 'rgba(0, 0, 0, 0.7)';
@@ -83,77 +19,46 @@ const getOrCreateTooltip = (chart) => {
     }
   
     return tooltipEl;
-  };
-  
-  const externalTooltipHandler = (context) => {
+};
+
+const externalTooltipHandler = (context) => {
+    //console.log("tooltip");
     // Tooltip Element
     const {chart, tooltip} = context;
     const tooltipEl = getOrCreateTooltip(chart);
   
     // Hide if no tooltip
     if (tooltip.opacity === 0) {
-      tooltipEl.style.opacity = 0;
-      return;
+        tooltipEl.style.opacity = 0;
+        return;
     }
   
     // Set Text
     if (tooltip.body) {
-      const titleLines = tooltip.title || [];
-      const bodyLines = tooltip.body.map(b => b.lines);
+        const bodyLines = tooltip.body.map(b => b.lines);
+        //console.log(bodyLines);
+        const tableHead = document.createElement('thead');
   
-      const tableHead = document.createElement('thead');
+        const tableBody = document.createElement('tbody');
+        bodyLines.forEach((body, i) => {
+            const colors = tooltip.labelColors[i];
+        
+            const imagem = document.createElement('IMG');
+            //console.log(tooltip.dataPoints[0].raw.id);
+            imagem.src = "molecules/" + String(tooltip.dataPoints[0].raw.id) + ".png"
+            tableBody.appendChild(imagem);
+        });
   
-      titleLines.forEach(title => {
-        const tr = document.createElement('tr');
-        tr.style.borderWidth = 0;
+        const tableRoot = tooltipEl.querySelector('table');
   
-        const th = document.createElement('th');
-        th.style.borderWidth = 0;
-        const text = document.createTextNode(title);
+        // Remove old children
+        while (tableRoot.firstChild) {
+            tableRoot.firstChild.remove();
+        }
   
-        th.appendChild(text);
-        tr.appendChild(th);
-        tableHead.appendChild(tr);
-      });
-  
-      const tableBody = document.createElement('tbody');
-      bodyLines.forEach((body, i) => {
-        const colors = tooltip.labelColors[i];
-  
-        const span = document.createElement('span');
-        span.style.background = colors.backgroundColor;
-        span.style.borderColor = colors.borderColor;
-        span.style.borderWidth = '2px';
-        span.style.marginRight = '10px';
-        span.style.height = '10px';
-        span.style.width = '10px';
-        span.style.display = 'inline-block';
-  
-        const tr = document.createElement('tr');
-        tr.style.backgroundColor = 'inherit';
-        tr.style.borderWidth = 0;
-  
-        const td = document.createElement('td');
-        td.style.borderWidth = 0;
-  
-        const text = document.createTextNode(body);
-  
-        td.appendChild(span);
-        td.appendChild(text);
-        tr.appendChild(td);
-        tableBody.appendChild(tr);
-      });
-  
-      const tableRoot = tooltipEl.querySelector('table');
-  
-      // Remove old children
-      while (tableRoot.firstChild) {
-        tableRoot.firstChild.remove();
-      }
-  
-      // Add new children
-      tableRoot.appendChild(tableHead);
-      tableRoot.appendChild(tableBody);
+        // Add new children
+        tableRoot.appendChild(tableHead);
+        tableRoot.appendChild(tableBody);
     }
   
     const {offsetLeft: positionX, offsetTop: positionY} = chart.canvas;
@@ -164,5 +69,65 @@ const getOrCreateTooltip = (chart) => {
     tooltipEl.style.top = positionY + tooltip.caretY + 'px';
     tooltipEl.style.font = tooltip.options.bodyFont.string;
     tooltipEl.style.padding = tooltip.options.padding + 'px ' + tooltip.options.padding + 'px';
-  };
-  
+};
+
+async function getData() {
+    const response = await fetch("molecules.csv");
+    //console.log(response);
+    const data = await response.text();
+    //console.log(data);
+    const rows = data.split('\n');
+    let dados = {
+        "id": [],
+        "smiles": [],
+        "pic50": [],
+        "bbb": [],
+        "mw": [],
+        "logp": [],
+        "sas": [],
+        "qed": []
+    }
+    const startLine = 6;
+    for(let i = startLine; i < rows.length - 1; i++) {
+        const row = rows[i].split(',');
+        dados.id.push(i - startLine + 1);
+        dados.smiles.push(row[0]);
+        dados.pic50.push(row[1]);
+        dados.bbb.push(row[2]);
+        dados.mw.push(row[3]);
+        dados.logp.push(row[4]);
+        dados.sas.push(row[5]);
+        dados.qed.push(row[6]);
+    }
+    //console.log(dados);
+    return dados;
+}
+
+async function plotData(id, xAxis, yAxis, config, larg, alt) {
+    const dados = await getData();
+    //console.log(dados[xAxis], dados[yAxis]);
+    let eixos = [];
+    for(let i = 0; i < dados.smiles.length; i++) {
+        eixos.push({
+        x: dados[xAxis][i],
+        y: dados[yAxis][i],
+        id: dados.id[i]
+        });
+    }
+    //console.log(config);
+    let config_local = JSON.parse(JSON.stringify(config)); //perguntar se é assim q se faz deepcopy
+    config_local.data.datasets[0].data = eixos;
+    config_local.options.scales.x.title.text = xAxis
+    config_local.options.scales.y.title.text = yAxis
+    config_local.options.plugins.tooltip.external = externalTooltipHandler; //quando uso o stringify + parse em cima perde esta propriedade n sei pq
+    //console.log(config_local);
+    let canvas = document.createElement("CANVAS");
+    canvas.id = id;
+    canvas.width = larg;
+    canvas.height = alt;
+    document.body.appendChild(canvas);
+    const ctx = canvas.getContext("2d");
+    const plot = new Chart(ctx, config_local);
+}
+
+
